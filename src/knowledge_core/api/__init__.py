severin-lemaignan/knowledge_core

commit eb0c97a1bef3e74c83a59e58201dd4d42292aa60

do not return blank nodes (BNodes) when querying the kb

src/knowledge_core/kb.py
@@ -19,7 +19,7 @@ except ImportError:
     sys.exit(1)
 
 from rdflib import Graph, Dataset
-from rdflib.term import Node, Literal, URIRef, Variable
+from rdflib.term import Node, BNode, Literal, URIRef, Variable
 from rdflib.namespace import Namespace, RDF, RDFS, OWL
 
 has_reasoner = False
@@ -872,6 +872,8 @@ class KnowledgeCore:
         If you were using anonymous variables (starting with ?__) in your query
         (eg generated by `pykb` when using a `*` wildcard), they will be
         renamed `var1`, `var2`, etc.
+
+        Note that RDF blank nodes (BNode) are not returned.
         """
 
         if not vars or (len(vars) == 1 and vars[0] == ""):
@@ -996,7 +998,9 @@ class KnowledgeCore:
         import pyparsing
 
         try:
-            return self.models[model].materialized_graph.query(q), q
+            res = self.models[model].materialized_graph.query(q)
+            no_bnode = [stmt for stmt in filter(lambda s: all(type(t) != BNode for t in s), res)]
+            return no_bnode, q
         except pyparsing.ParseException:
             raise KbServerError("Syntax error while parsing SPARQL query:\n%s" % q)
 

commit 7bccb90b9d03d5ad1f5d86a99401cd353af2b22d

fix sparql() that was broken since eb0c97a1bef3e74

src/knowledge_core/api/__init__.py
@@ -102,7 +102,8 @@ class KB:
         if isinstance(pattern, str):
             pattern = [pattern]
 
-        evt = self._events_srv(patterns=pattern, one_shot=one_shot, models=models)
+        evt = self._events_srv(
+            patterns=pattern, one_shot=one_shot, models=models)
 
         evt_relay = EvtRelay(callback)
         rospy.Subscriber(evt.topic, String, evt_relay.callback, queue_size=10)
@@ -332,3 +333,12 @@ class KB:
             else:
                 res.append(tok)
         return tuple(res)
+
+    def sparql(self, query, models=[]):
+
+        res = self._sparql_srv(query=query, models=models)
+
+        if not res.success:
+            raise KbError(res.error_msg)
+
+        return json.loads(res.json)

src/knowledge_core/kb.py
@@ -875,19 +875,20 @@ class KnowledgeCore:
         serialization of SPARQL Results
         (https://www.w3.org/TR/2013/REC-sparql11-results-json-20130321/)
 
-        Example:
+        Example (using the `knowledge_core.api.KB` Python wrapper):
         ```
         >>> kb += ["myself age 40"]
         >>> kb.sparql("SELECT ?age WHERE { :myself :age ?age . }")
         {'results':
             {'bindings': [
                 {'age':
-                    {'type': 'literal',
+                    {'type': 'typed-literal',
                      'value': '40',
                      'datatype': 'http://www.w3.org/2001/XMLSchema#integer'}
                 }]
             },
           'head': {'vars': ['age']}
+          'query': # original query
          }
 
         """
@@ -901,7 +902,7 @@ class KnowledgeCore:
 
         logger.info("Executing SPARQL query in model: %s\n%s" % (model, query))
 
-        sparql_res, query = self._sparql(model, query)
+        sparql_res, query = self._sparql(model, query, raw=True)
 
         import json
 
@@ -1055,7 +1056,17 @@ class KnowledgeCore:
     ################################################################################
     ################################################################################
 
-    def _sparql(self, model, query):
+    def _sparql(self, model, query, raw=False):
+        """
+
+        :param raw: if True, returns the raw SPARQLResult object; if False,
+        returns a list of triples fulfulling the query.
+
+        :return: result of the query (see `raw` parameter) and original query,
+        augmented with standard SPARQL prefixes.
+
+        """
+
         q = SPARQL_PREFIXES  # TODO: as a (potential?) optimization, pass initNs to graph.query, instead of adding the PREFIX strings to the query
         q += query
 
@@ -1065,10 +1076,17 @@ class KnowledgeCore:
 
         try:
             res = self.models[model].materialized_graph.query(q)
-            no_bnode = [stmt for stmt in filter(lambda s: all(type(t) != BNode for t in s), res)]
-            return no_bnode, q
+
+            if raw:
+                return res, q
+            else:
+                no_bnode = [stmt for stmt in filter(lambda s: all(type(t) != BNode for t in s), res)]
+                return no_bnode, q
         except pyparsing.ParseException:
             raise KbServerError("Syntax error while parsing SPARQL query:\n%s" % q)
+        except TypeError as te:
+            raise KbServerError("Syntax error while parsing SPARQL query:\n%s\nException was: %s" % (q, te))
+
 
     def named_variables(self, vars):
         """